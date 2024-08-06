99f7f1030d52d214d64e84ead6e35055e64787c3
njivago <snag.black@gmail.com>
2024-08-06 10:04:50 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  basePath: isProd ? "commercial-web" : '',
   output: "export",
 };
 
